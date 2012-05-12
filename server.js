67c03737b95b793bcf59106d344ca3586dd902fa
Kyle Chiu <krypes+github@gmail.com>
2012-05-12 18:32:10 -0400

    messages are now logged to a mongo db

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 var app = require('express').createServer()
+  , mongoose = require('mongoose')
   , io = require('socket.io').listen(app);
 
+var Schema = mongoose.Schema;
+var ChatSchema = new Schema({
+	time: {type: Date},
+	user: {type: String},
+	message: {type: String, trim: true}
+});
+var ChatModel = mongoose.model('Chat', ChatSchema);
+
+mongoose.connect('mongodb://localhost/lvg');
+
+var fetchRecentMessages = function(callback) {
+	var chatModel = mongoose.model('Chat');
+	chatModel
+	  .find()
+	  .sort('time', -1) // descending
+	  .limit(5)
+	  .run(callback);
+};
+var logMessage = function(data) {
+	var chatModel = mongoose.model('Chat');
+	new ChatModel({time: new Date(), user: data.user, message: data.message}).save();
+};
 app.listen(80);
 
 app.get('/', function (req, res) {
@@ -8,6 +31,7 @@ app.get('/', function (req, res) {
 });
 
 io.sockets.on('connection', function (socket) {
+	
 	socket.emit('message', {
 		user: 'server',
 		message: 'MOTD: some bullshit' 
@@ -20,12 +44,24 @@ io.sockets.on('connection', function (socket) {
 		console.log(data);
 		socket.broadcast.emit('message', data);
 		socket.emit('message', data);
+		logMessage(data);
 	});
 	socket.on('disconnect', function() {
 		socket.broadcast.emit('message', {
 			user: 'server',
 			message: 'someone quit! well fuck them' 
 		});
+	});
+	fetchRecentMessages(function(err,messages) {
+		console.log('pushing history');
+		console.log(err);
+		console.log(messages);
+		for(i=messages.length-1; i >=0; --i)
+		{
+			var message = messages[i];
+			console.log(message);
+			socket.emit('message', message);
+		}
 	});
 });
 
