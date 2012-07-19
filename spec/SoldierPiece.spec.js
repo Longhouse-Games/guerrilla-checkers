d3fe2bd45b227f73416e1513917a18d6410390d5
kjchiu <krypes+github@gmail.com>
2012-07-19 19:17:26 -0400

    Absolutely the worst example of bdd. I'm so sorry T_T

--- spec/SoldierPiece.spec.js
+++ spec/SoldierPiece.spec.js
@@ -1,11 +1,26 @@
 require(['./lib/checkers'], function(Checkers) {
 describe('A guerilla-checkers game', function() {
 	describe('which has just begun', function() {
-		var board = new Checkers.GameState();
+		var board; 
+		beforeEach(function() {
+			board = new Checkers.GameState();
+		});
 		it('has a soldier piece at (3,2)', function() {
 			var position = new Checkers.Position(3,2);
 			expect(board.soldierPieceAt(position)).not.toBeNull();
 		});
+
+		it('the soldier piece at (3,2) may move to (4,3)', function() {
+			var piecePosition = new Checkers.Position(3,2);
+			var destination = new Checkers.Position(4,3);
+			expect(board.isValidSoldierMove(piecePosition, destination)).toBe(true);
+		});
+		it('must begin with a guerilla move', function() {
+			var position = new Checkers.Position(3,2);
+			var destination = new Checkers.Position(4,3);
+			expect(board.isValidSoldierMove(position, destination)).toBe(false);
+		});
 	});
+	
 });
 });
