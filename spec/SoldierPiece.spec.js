8ac33516bf7e3e9115dbd0dae956b31d42ae776a
kjchiu <krypes+github@gmail.com>
2012-07-26 22:20:49 -0400

    Updated soldier spec. #4

--- spec/SoldierPiece.spec.js
+++ spec/SoldierPiece.spec.js
@@ -1,20 +1,53 @@
 require(['./lib/checkers'], function(Checkers) {
-describe('A Guerrilla Checkers board', function() {
+describe('A soldier piece', function() {
+	var Position = Checkers.Position;
+	var GameState = Checkers.GameState;
 	var board;
 	beforeEach(function() {
-		board = new Checkers.GameState([], false);
+		board = new GameState([], false);
 	});
-	describe('with a soldier piece at (5,4)', function() {
+	describe('at (5,4)', function() {
 		beforeEach(function() {
-			board = new Checkers.GameState([new Checkers.Position(5,4)], false);
+			board = new GameState([new Position(5,4)], false);
 		});
-		it('the soldier piece at (5,4) may move to (6,5)', function() {
-			var piecePosition = new Checkers.Position(5,4);
-			var destination = new Checkers.Position(6,5);
+		it('may move to (6,5)', function() {
+			var piecePosition = new Position(5,4);
+			var destination = new Position(6,5);
 			var soldierPiece = board.soldierPieceAt(piecePosition);
 			expect(soldierPiece).not.toBeNull();
 			expect(board.isValidSoldierMove(soldierPiece, destination)).toBe(true);
 		});
+
+
+
+		describe('adjacent to a guerilla piece', function() {
+			beforeEach(function() {
+				var position = new Position(5,4);
+				board.placeGuerrillaPiece(position);
+				assert.ok(board.guerrillaPieceAt(position));
+			});
+
+			it('can only move to capture the guerilla', function() {
+				var piece = board.soldierPieceAt(new Position(5,4));
+				var positions =  [
+					new Position(piece.position.x - 1, piece.position.x + 1),
+					new Position(piece.position.x - 1, piece.position.x - 1),
+					new Position(piece.position.x + 1, piece.position.x - 1)
+				];
+				for(idx=0; i < positions.length; ++i) {
+					expect(board.moveSoldierPiece(piece, positions[i])).toBe(false);
+				}
+				expect(board.moveSoldierPiece(piece, new Position(6,5))).toBe(true);
+			});
+
+			it('when capturing the guerrilla, removes it from the board', function() {
+				var piece = board.soldierPieceAt(new Position(5,4));
+				board.moveSoldierPiece(piece, new Position(6,5));
+				expect(board.guerilla(new Poosition(5,4))).toBeNull();
+			});
+
+		});
 	});
+
 });
 });
